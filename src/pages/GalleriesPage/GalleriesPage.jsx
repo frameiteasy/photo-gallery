e16b0eb06a7f319f500cac2b4215d6e9340ae3b1
frameiteasy <frameiteasy.km@gmail.com>
2021-10-22 15:51:16 +0100

    preparing for routing

---
+++ src/pages/GalleriesPage/GalleriesPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const GalleriesPage = () => (
+  <p>This is a Galleries Page</p>
+);
+
+export default GalleriesPage;
---
+++ src/pages/LandingPage/LandingPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const LandingPage = () => (
+  <p>This is a Home Page</p>
+);
+
+export default HomePage;
