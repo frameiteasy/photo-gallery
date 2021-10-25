e96fddd794f512699b133180560765f7f17211c8
frameiteasy <frameiteasy.km@gmail.com>
2021-10-25 11:53:40 +0100

    LandingPage is working. Still problem with GalleriesPage

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom'; 
+import LandingPage from './pages/LandingPage/LandingPage.jsx'
+import GalleriesPage from './pages/GalleriesPage/GalleriesPage.jsx';
+
+export default function App() {
+  return(
+    <BrowserRouter>
+        <Route exact path="/" component={ LandingPage }/>
+        <Route path="/galleries" component={ GalleriesPage }/>
+    </BrowserRouter>  
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App/App.jsx';
+import App from './App.jsx';
 
 ReactDOM.render(
-  <App name="Konrad" message="hello"/>, 
+  <App />, 
   document.getElementById('root')
 );
