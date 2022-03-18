e10d69eb793b2e1a6d19f5d1095dd11453d1e9e8
Konrad Marciniak <konrad@Konrads-MacBook-Air.local>
2022-03-18 17:25:31 +0000

    adding style flexibility

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import PhotoTile from './components/PhotoTile/PhotoTile.jsx';
-import "./index.scss";
+import "./App.scss";
 
 const App = () => {
     return (
     <>
-        <div classNmae="wrapper">
-            <h1 className="mainHeader"> Portfolio</h1>
-            <h2 className="secondaryHeader">Konrad Marciniak</h2>
+        <h1 className="mainHeader"> Portfolio</h1>
+        <h2 className="secondaryHeader">Konrad Marciniak</h2>
+        <div className="container">
             <PhotoTile photo="Photo1" title="Photo title" description="Photo description"/>
             <PhotoTile photo="Photo2" title="Photo title" description="Photo description"/>
             <PhotoTile photo="Photo3" title="Photo title" description="Photo description"/>
             <PhotoTile photo="Photo4" title="Photo title" description="Photo description"/>
             <PhotoTile photo="Photo5" title="Photo title" description="Photo description"/>
+            <PhotoTile photo="Photo6" title="Photo title" description="Photo description"/>
+            <PhotoTile photo="Photo7" title="Photo title" description="Photo description"/>
         </div>
         
     </>
--- src/components/PhotoTile/PhotoTile.jsx
+++ src/components/PhotoTile/PhotoTile.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import "./photoTile.scss";
 
+/*
+ * Component should send a request for a picture to the server (callback)
+*/
+
 function PhotoTile({photo, title, description}) {
     return(
         <div className="photoTile__wrapper">
+            <div className="photoTile__image">
+
+            </div>
             <h3>{title}</h3>
             <h4>{photo}</h4>
             <p>{description}</p>
