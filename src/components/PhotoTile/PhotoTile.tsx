61cb62076eb24e77ad19453436b85d7ddd77fd8f
Konrad Marciniak <konrad@Konrads-MacBook-Air.local>
2022-09-11 17:16:34 +0100

    adding assets

--- src/components/PhotoTile/PhotoTile.tsx
+++ src/components/PhotoTile/PhotoTile.tsx
@@ -10,10 +10,11 @@ const PhotoTile: React.FC<Photo> = (photo: Photo) => {
     return(
         <div className="photoTile__wrapper">
             <div className="photoTile__image">
-                <h3>{photo.name}</h3>
-                <h4>{photo.title}</h4>
-                <p>{photo.description}</p>
             </div>
+
+            <h3>{photo.name}</h3>
+            <h4>{photo.title}</h4>
+            <p>{photo.description}</p>
         </div>
     )
 }
