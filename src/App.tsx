f119e11812080b0fd6e13ef68a32d222d48d87b6
Konrad Marciniak <konrad@Konrads-MacBook-Air.local>
2022-09-24 15:51:39 +0100

    adding Login route

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,7 @@ import Home from './pages/home/Home';
 import Albums from './pages/albums/Albums';
 import About from './pages/about/About';
 import Contact from './pages/contact/Contact';
-
-
+import Login from './pages/login/Login';
 
 function App() {
   return (
@@ -18,6 +17,7 @@ function App() {
         <Route path="/albums" element={<Albums/>}></Route>
         <Route path="/about" element={<About/>}></Route>
         <Route path="/contact" element={<Contact/>}></Route>
+        <Route path="/login" element={<Login/>}></Route>
       </Routes>    
     </>
   );
