c0a9c2bd18fbb22b67e25dca0d3f9e5c2c9a7dc2
Konrad Marciniak <konrad@Konrads-MacBook-Air.local>
2022-10-11 19:05:35 +0100

    adding MenuItem component

--- src/components/NavBar/Navbar.tsx
+++ src/components/NavBar/Navbar.tsx
@@ -1,27 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import "./navbar.scss";
+import getMenuItems from './menuitems';
+import { MenuItem } from '../../gtypes/menuitem';
+
+const menuItems: MenuItem[] = getMenuItems();
 
 const Navbar: React.FC = () => {
     return(
         <nav className='nav'>
             <div className='container'>
                 <h1 className='navbar__site-title'>
-                    <Link to='/'>Josef Chilersa Photography</Link>
+                    <Link to='/' className='navbar__logo'>Josef Chilersa Photography</Link>
                 </h1>
                 <ul>
-                    <li>
-                        <Link to='/' className="active">Home</Link>
-                    </li>
-                    <li>
-                        <Link to='/albums'>Albums</Link>
-                    </li>
-                    <li>
-                        <Link to='/contact'>Contact</Link>
-                    </li>
-                    <li>
-                        <Link to='/about'>About</Link>
-                    </li>
+                    {
+                        menuItems.map( (item, index) => (
+                            <li>
+                                <Link to={item.path} className={item.className}>
+                                    {item.name}
+                                </Link>
+                            </li>
+                            )
+                        )
+                    }
                 </ul>
             </div>
         </nav>
---
+++ src/components/NavBar/menuitems.tsx
@@ -0,0 +1,7 @@
+import menuItems from './menuitems.json'; 
+
+const getMenuItems = () => {
+    return menuItems;
+}
+
+export default getMenuItems;
