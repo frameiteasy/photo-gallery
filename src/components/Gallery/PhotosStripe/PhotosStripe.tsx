d1167a4a85dbed0e245c8a58a2b8c0bcd3653133
Konrad Marciniak <konrad@Konrads-MacBook-Air.local>
2023-03-03 17:45:31 +0000

    developing stripe

--- src/components/Gallery/PhotosStripe/PhotosStripe.tsx
+++ src/components/Gallery/PhotosStripe/PhotosStripe.tsx
@@ -4,7 +4,11 @@ import './photosStripe.scss';
 
 const PhotosStripe: React.FC<Photo[]> = (photos: Photo[]) => {
   console.log("Photos' Stripe", photos);
-  return <div id="gallerystripe__container">Photos Stripe</div>;
+  const miniatures = [];
+  for (let i = 0; i < 6; i++) {
+    miniatures.push(<div id="gallerystripe__mini_picture">{i}</div>);
+  }
+  return <div id="gallerystripe__container">{miniatures}</div>;
 };
 
 export default PhotosStripe;
